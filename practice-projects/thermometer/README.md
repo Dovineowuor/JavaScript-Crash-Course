0678cda11e246d678af01d24a990b2d9ddeb6c35
Doveowuor <>
2023-03-09 14:16:17 +0000

    thermometer

---
+++ practice-projects/thermometer/README.md
@@ -0,0 +1,2 @@
+# The thermometer Project
+This repository showcases the thermometer [`app.js`](thermometer/app.js) which helps in convert temperature units from one to another.
---
+++ practice-projects/thermometer/app.js
@@ -0,0 +1,20 @@
+// var kelvin = 293;
+// var celsius ;
+// The variable kelvin is constant
+// var temperature = 0;
+// switch (temperature = 273) {
+//   case temperature: 293
+//     console.log(`The temperature in degree celsius of 293 = ${kelvinScale} - ${temperature}`);
+//     break;
+// }
+// var celsius = 20;
+var kelvin = 273;
+var celsius = kelvin - 273;
+var ferenheit = celsius * (9 / 5) + 32;
+// Farenheit conversion from celsius
+farenheit = Math.floor(ferenheit);
+console.log(`The temperature is ${ferenheit} degrees Fahrenheit.`); // printing the output in a stractured manner
+
+var Newton = celsius * (33 / 100);
+newton = Math.floor(Newton);
+console.log(`The temperature is ${newton} N.`);
