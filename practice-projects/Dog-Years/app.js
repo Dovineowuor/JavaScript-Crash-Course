21abfeaabb86373e8c2c8ed48ec06c8c4795ef0b
Doveowuor <>
2023-03-09 15:26:18 +0000

    dog-years

---
+++ practice-projects/Dog-Years/app.js
@@ -0,0 +1,18 @@
+const myAge = 20;
+//  Keeps the age. Can be changes by re-declaration or re-assignment
+var earlyYears = 2;
+// Keeps the earlyYears value 2
+var earlyYears = earlyYears * 10.5; // The  reassignment of earlyYears variable
+var laterYears = myAge - 2;
+// Sets the result equal to a variable called laterYears. The value being variable, it may be changed later.
+laterYears *= 4;
+// An operation to calculate the dog years
+console.log(`Early Years = ${earlyYears}`);
+console.log(`Late years = ${laterYears}`)
+// Test the code
+
+myAgeInDogYears = earlyYears + laterYears
+// Declare a new variable resulting from summation of later and early years
+myName = "NAME".toLowerCase()
+// Declarea variable myName Declaring a string of my name NAME.
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
