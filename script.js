79b8420c6a813956f474018b46fbb7b349e72530
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2023-03-09 11:27:01 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// // var, let, const
+// const score;
+// score = 10;
+// const name = Dove;
+// const hello = `I am ${name} and scored ${score}`;
+// console.log(hello);
+
+// const s = 'Hello There!';
+// console.log(s);
+// console.log();
+
+let name = ['Dove, K, O'];
+console.log(name);
+name.push('fest');
+name.
