2385a438553aa6ac3c7af9991f87f3e22a5b1541
Doveowuor <>
2023-03-13 11:38:20 +0000

    If else

---
+++ practice-projects/conditionals/README.md
@@ -0,0 +1,4 @@
+# JavaScript Conditionals
+**This repository showcases conditional insinuations in JavaScript.**
+
+This repository was built for practice purposes. All the files and scripts are written from scratch during codeacademy 
---
+++ practice-projects/conditionals/tenary-operations.js
@@ -0,0 +1,33 @@
+let isLocked = false;
+
+// if (isLocked) {
+//   console.log('You will need a key to open the door.');
+// } else {
+//   console.log('You will not need a key to open the door.');
+// }
+
+// Tenary Operation
+isLocked ? console.log('You will need a key to open the door.') : console.log('You will not need a key to open the door.');
+let isCorrect = true;
+
+// IF / ELSE
+// if (isCorrect) {
+//   console.log('Correct!');
+// } else {
+//   console.log('Incorrect!');
+// }
+// Tenary operation
+
+isCorrect ? console.log('Correct!') : console.log('Incorrect!');
+
+let favoritePhrase = 'Love That!';
+
+//  IF / ELSE
+// if (favoritePhrase === 'Love That!') {
+//   console.log('I love that!');
+// } else {
+//   console.log("I don't love that!");
+// }
+
+// Tenary Operation
+favoritePhrase === "Love That!" ? console.log("I love that!") : console.log("I don't love that!");
